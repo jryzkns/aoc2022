8d5863df33e1b79e130c5a5f5a22cdcba3f661af
jryzkns <jryzkns@gmail.com>
2022-12-07 00:12:15 -0800

    day 7 done

---
+++ 7/72.cr
@@ -0,0 +1,93 @@
+class Node
+    def initialize( name : String,
+                    parent : ( Node | Nil ) = nil )
+        @name = name
+        @files = Hash( String, Int32 ).new
+        @dirs = Hash( String, Node ).new
+        @parent = parent
+    end
+
+    def newDir( name : String )
+        node = Node.new( "#{@name}/#{name}", self )
+        @dirs[ name ] = node
+    end
+
+    def newFile( name : String, size : Int32 )
+        @files[ "#{@name}/#{name}" ] = size
+    end
+
+    def parent
+        @parent.not_nil!
+    end
+
+    def dirs
+        @dirs
+    end
+
+    def intoDir( name : String )
+        @dirs[ name ]
+    end
+
+    def shallowSize
+        @files.each_value.sum
+    end
+
+    def deepSize : Int32
+        self.shallowSize + @dirs.each_value.map { |node| node.deepSize }.sum
+    end
+end
+
+
+root = Node.new ""
+curr = root
+
+File.open( "input" ).each_line do | line |
+    if line == "$ ls"
+        next # there isn't actually anything to process here
+    elsif line[ 0..3 ] == "$ cd"
+        dst = line[ 5.. ]
+        if dst == ".."
+            curr = curr.parent
+        else
+            curr = curr.intoDir( dst )
+        end
+    else # any other possible line structure is just ls output
+        if dirMatch = /dir (\w+)/.match( line )
+            dirName = dirMatch.not_nil!.captures[ 0 ]
+            curr.newDir( dirName.not_nil! )
+        end
+        if fileMatch = /(\d+) (.+)/.match( line )
+            fileSize, fileName = fileMatch.not_nil!.captures
+            curr.newFile( fileName.not_nil!, fileSize.not_nil!.to_i )
+        end
+    end
+end
+
+def traverse( root : Node )
+    nodes = Array( Node ).new
+    queue = Array( Node ).new
+    queue << root
+    while queue.size != 0
+        node = queue.shift
+        nodes << node
+        node.dirs.each_value { | n | queue << n }
+    end
+
+    return nodes
+end
+
+nodes = traverse( root )
+
+dirSizes = nodes.map { | node | node.deepSize }
+
+maxUsage = 70000000 - 30000000
+used = root.deepSize
+
+minDeletable = maxUsage
+dirSizes.each do | size |
+    if used - size <= maxUsage
+        minDeletable = Math.min( minDeletable, size )
+    end
+end
+
+puts minDeletable
